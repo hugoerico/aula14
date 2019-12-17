816aea5be64db282570476e5b3389ca2c2af9ac8
Hugo Erico <hugo.019@hotmail.com>
2019-12-17 02:37:16 -0300

    melhorando a organização

--- felipe_exercicio45678.py
+++ felipe_exercicio45678.py
@@ -2,19 +2,25 @@ def celsius(valor):
     valor1=(valor*9/5)+32
     return f'o valor {valor} celsius convertido para fahrenheit é: {valor1}'
 
-print(celcius(32))
+print(celsius(int(input("digite um valor inteiro "))))
 
 def repetir(valor,valor1):
     valor2= valor.count(valor1)
-    return f'o {valor1} se repeti: {valor2}'
+    return f'o {valor1} se repete: {valor2}'
 
-print(repetir('hugo hugo hugo, erico','h'))
+valor=input('digite palavras separadas por espaço ')
+valor1=input('digite a letra que quer saber a quantidade que se repete  ')
+print(repetir(valor,valor1))
 
 def media(valor,valor1,valor2,valor3):
-    valor4= valor+valor1+valor2+valor3/4
+    valor4= (valor+valor1+valor2+valor3)/4
+    
     return f'a media das 4 notas é: {valor4}'
-
-print(media(5,8,5,7))
+valor=float(input('digite o 1º valor '))
+valor1=float(input('digite o 2º valor '))
+valor2=float(input('digite o 3º valor '))
+valor3=float(input('digite o 4º valor '))
+print(media(valor,valor1,valor2,valor3))
 
 
 def maior():
